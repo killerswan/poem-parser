60cdd1e370348856e19c06f92ed1dd07e524a54b
Kevin Cantu <me@kevincantu.org>
2015-08-12 02:16:31 -0700

    make a test read the digits

--- poemvalidator/test/poemvalidator_test.exs
+++ poemvalidator/test/poemvalidator_test.exs
@@ -1,7 +1,32 @@
 defmodule PoemvalidatorTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def read_all(path) do
+    file = File.open!(path, [:read, :utf8])
+    digits = IO.read(file, :line) |> String.strip
+    File.close(file)
+    digits
+  end
+
+  test "pi is being read from file" do
+    digits = read_all("../pi-500.txt")
+    assert String.starts_with? digits, "3.1415" 
+
+    first_few = digits |> String.slice 0..6
+    assert first_few != "3.11111"
+
+    # compare these syntaxes ways
+    first_few = digits |> String.slice 0..6
+    assert first_few == "3.14159"
+
+    first_few = digits |> String.slice(0..6)
+    assert first_few == "3.14159"
+
+    first_few = String.slice digits, 0..6
+    assert first_few == "3.14159"
+
+    first_few = String.slice(digits, 0..6)
+    assert first_few == "3.14159"
+
   end
 end
